8f6ae30234569b10cbbe1cb6c458657392df81fc
Tom <wellurban@gmail.com>
2021-06-19 17:39:13 +1200

    Start intro vignette

---
+++ explore-sfnetworks.R
@@ -0,0 +1,10 @@
+# Get familiar with sfnetworks
+
+# based on https://luukvdmeer.github.io/sfnetworks/articles/structure.html
+
+library(sfnetworks)
+library(sf)
+library(tidygraph)
+library(tidyverse)
+library(igraph)
+
