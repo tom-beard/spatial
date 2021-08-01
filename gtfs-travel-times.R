5689c7f25132e74434c805fa356ac4660f0f6cf3
Tom <wellurban@gmail.com>
2021-08-01 20:58:02 +1200

    Test scaling to large batches of stops

--- gtfs-travel-times.R
+++ gtfs-travel-times.R
@@ -35,6 +35,7 @@ library(here)
 library(hms)
 library(glue)
 library(tidytransit)
+library(tictoc)
 
 gtfs_dir <- here("gtfs")
 # gtfs_file <- "wellington-20210503.zip"
@@ -65,5 +66,49 @@ tts %>%
 
 # using stop names can be awkward and isn't guaranteed to be unique.
 # thus, raptor() might be a better option
+
+
+# Wellington with raptor() ------------------------------------------------
+
+tts_raptor <- raptor(stop_times, transfers = raw_gtfs$transfers,
+                     stop_ids = c("7711"), keep = "shortest")
+
+tts_raptor_df <- tts_raptor %>% 
+  as_tibble() %>% 
+  left_join(raw_gtfs$stops %>% select(stop_id, to_stop_lat = stop_lat, to_stop_lon = stop_lon),
+            by = c("to_stop_id" = "stop_id"))
+
+tts_raptor_df %>% 
+  ggplot() +
+  geom_point(aes(x = to_stop_lon, y = to_stop_lat, color = travel_time)) +
+  coord_map()
+
+
+# test timing for multiple stops ------------------------------------------
+
+from_stops <- raw_gtfs$stops %>% head(1000) %>% pull(stop_id)
+# from_stops <- raw_gtfs$stops$stop_id %>% unique()
+
+tic()
+tts_raptor <- raptor(stop_times, transfers = raw_gtfs$transfers,
+                     stop_ids = from_stops, keep = "shortest")
+toc()
+# 100: 1.32s
+# 1000: 3.44s
+# 2000 or more: "If you are sure you wish to proceed, rerun with allow.cartesian=TRUE"
+# might need to map over batches instead: easiest way might be via group_modify():
+# https://dplyr.tidyverse.org/reference/group_map.html
+
+tts_raptor_df <- tts_raptor %>% 
+  as_tibble() %>% 
+  left_join(raw_gtfs$stops %>% select(stop_id, to_stop_lat = stop_lat, to_stop_lon = stop_lon),
+            by = c("to_stop_id" = "stop_id"))
+
+tts_raptor_df %>% 
+  filter(from_stop_id != to_stop_id)
+
+# migth need to be careful with hierarchical stations and bus/train transfers
+raw_gtfs$stops %>% filter(stop_id == "1001") %>% glimpse()
+raw_gtfs$stops %>% filter(startsWith(stop_name, "Paraparaumu Station")) %>% View()
 
 
