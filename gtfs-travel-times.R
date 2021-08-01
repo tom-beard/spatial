ad7be01558f8d567ff55360999c846faa774fbec
Tom <wellurban@gmail.com>
2021-08-01 18:35:23 +1200

    Use latest Wellington GTFS for testing

--- gtfs-travel-times.R
+++ gtfs-travel-times.R
@@ -1,3 +1,6 @@
+
+# travel_times() example --------------------------------------------------
+
 # Example from http://tidytransit.r-transit.org/reference/travel_times.html
 
 library(tidyverse)
@@ -19,3 +22,48 @@ tts %>% filter(to_stop_name == "Queensboro Plaza") %>% pull(travel_time) %>% hms
 # this can be expanded to isochrone maps
 ggplot(tts) +
   geom_point(aes(x = to_stop_lon, y = to_stop_lat, color = travel_time))
+
+# travel_times() is a wrapper around raptop() (see http://tidytransit.r-transit.org/reference/raptor.html)
+
+
+# Wellington example ------------------------------------------------------
+
+library(tidyverse)
+library(fs)
+library(lubridate)
+library(here)
+library(hms)
+library(glue)
+library(tidytransit)
+
+gtfs_dir <- here("gtfs")
+# gtfs_file <- "wellington-20210503.zip"
+# gtfs_file <- "wellington-20210126.zip"
+gtfs_file <- "wellington-20210725.zip"
+
+raw_gtfs <- read_gtfs(path(gtfs_dir, gtfs_file))
+raw_gtfs$feed_info
+raw_gtfs$transfers # seems to have plenty of transfers
+raw_gtfs$calendar %>% count(start_date, end_date)
+
+raw_gtfs$stops %>%
+  filter(startsWith(stop_name, "Willis")) %>% 
+  select(stop_id, stop_name)
+
+stop_times <- filter_stop_times(raw_gtfs, "2021-07-27", 7*3600, 9*3600)
+
+tts <- travel_times(stop_times, "Willis Street - Abel Smith Street", return_coords = TRUE)
+tts <- tts %>% filter(travel_time <= 3600)
+tts %>% count(transfers)
+
+tts %>% filter(to_stop_name == "Porirua Station - Stop B") %>% pull(travel_time) %>% hms::hms()
+
+tts %>% 
+  ggplot() +
+  geom_point(aes(x = to_stop_lon, y = to_stop_lat, color = travel_time)) +
+  coord_map()
+
+# using stop names can be awkward and isn't guaranteed to be unique.
+# thus, raptor() might be a better option
+
+
