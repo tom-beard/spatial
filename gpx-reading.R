f0071b8bdb26517150f447e53434278d9b70a3b0
Tom <wellurban@gmail.com>
2022-04-27 20:08:36 +1200

    Force widget to open in browser

--- gpx-reading.R
+++ gpx-reading.R
@@ -174,8 +174,14 @@ paths_centre <- path_3d_sf %>%
   st_centroid() %>% 
   st_coordinates()
 
-deckgl(zoom = 9.5, pitch = 45, longitude = paths_centre[1], latitude = paths_centre[2],
-       width = 1200, height = 900) %>%
+view_widget <- function(widget, widget_path = here("temp_widget.html")) {
+  htmlwidgets::saveWidget(widget, file = widget_path)
+  browseURL(widget_path)
+}
+
+deckgl(zoom = 9.5, pitch = 45,
+                        longitude = paths_centre[1], latitude = paths_centre[2],
+                        width = 1200, height = 900) %>%
   add_path_layer(
     data = path_3d_sf,
     getPath = JS("d => d.geometry.coordinates"),
@@ -185,5 +191,6 @@ deckgl(zoom = 9.5, pitch = 45, longitude = paths_centre[1], latitude = paths_cen
     getTooltip = ~name,
     getWidth = 3
   ) %>%
-  add_basemap(style = use_carto_style("positron"))
+  add_basemap(style = use_carto_style("positron")) %>% 
+  view_widget()
 
